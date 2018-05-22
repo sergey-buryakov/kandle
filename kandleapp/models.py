2d51ea5bc76a9c8f55d63dffce07e9235a8cfc0b
Max <forlivak@gmail.com>
2018-05-22 23:33:42 +0300

    Created models. Done migration.

--- kandleapp/models.py
+++ kandleapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	userId = models.AutoField(db_column='userId', primary_key=True)
+	login = models.TextField()
+	password = models.TextField()
+	email = models.EmailField()
+class Event(models.Model):
+	eventId = models.UUIDField()
+	name = models.TextField()
+	description = models.TextField()
+	startVote = models.DateTimeField()
+	finishVote = models.DateTimeField()
+	userId = models.ForeignKey(User, on_delete = models.CASCADE)
+	
+class Date(models.Model):
+	dateId = models.AutoField(db_column='dateId', primary_key=True)
+	date = models.DateField()
+	startTime = models.TimeField()
+	finishTime = models.TimeField()
+	eventId = models.ForeignKey(Event, on_delete = models.CASCADE)
+	users = models.ManyToManyField(User)
+	
