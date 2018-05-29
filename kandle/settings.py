28e29c4217b7b15d00ae8d8d253b91b95e90b2cb
Daria Mozil <dashamozil@gmail.com>
2018-05-29 13:16:17 +0300

    changes in settings

--- kandle/settings.py
+++ kandle/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
