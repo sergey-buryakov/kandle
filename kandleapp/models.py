ae7db0f4d90d41ae7d4c157a1aefe47d9d4f4715
Max <forlivak@gmail.com>
2018-05-29 22:39:51 +0300

    changed model

--- kandleapp/models.py
+++ kandleapp/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Event(models.Model):
-	eventId = models.UUIDField()
+	eventUrl = models.TextField()
 	name = models.TextField()
 	description = models.TextField()
-	startVote = models.DateTimeField()
-	finishVote = models.DateTimeField()
+	startVote = models.DateTimeField(null=False)
+	finishVote = models.DateTimeField(null=False)
 	userId = models.ForeignKey(User, on_delete = models.CASCADE)
 	
 class Date(models.Model):
 	dateId = models.AutoField(db_column='dateId', primary_key=True)
-	date = models.DateField()
-	startTime = models.TimeField()
-	finishTime = models.TimeField()
+	date = models.DateField(null=False)
+	startTime = models.TimeField(null=False)
+	finishTime = models.TimeField(null=False)
 	eventId = models.ForeignKey(Event, on_delete = models.CASCADE)
 	users = models.ManyToManyField(User)
 	
