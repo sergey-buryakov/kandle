0baaa88ca524b516c2fae5badda14921ef974a61
sergey <sergey12.01.98@gmail.com>
2018-06-07 17:15:05 +0300

    added classes to form sign_up

--- kandleapp/forms.py
+++ kandleapp/forms.py
@@ -14,6 +14,14 @@ class Sign_up_form(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'})
+        }
 
 class CreateEventForm(ModelForm):
     class Meta:
